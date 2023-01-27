7714b07d2d1880587941b4ae5c5842022d71ca21
janani-ks <123439325+janani-ks@users.noreply.github.com>
2023-01-27 16:53:49 +0530

    Add files via upload

---
+++ Projects/src/main/java/Project5.java
@@ -0,0 +1,28 @@
+class BasicPoint implements Cloneable {
+	String x,y;
+	BasicPoint(String x1,String y1){
+		this.x=x1;
+		this.y=y1;
+	}
+	public Object clone()throws CloneNotSupportedException{  
+		return super.clone();  
+		}  
+	static void Clone(){
+		try {
+		BasicPoint s = new BasicPoint("4.6","8");
+		System.out.println((s.x).equals(s.y)); 
+		BasicPoint s1 = (BasicPoint)s.clone();
+		System.out.println(s1.x+" "+s1.y);
+		}
+		catch(CloneNotSupportedException c) {}
+}
+}
+public class Project5
+{
+	public static void main(String[] args) {
+		BasicPoint.Clone(); 
+		
+		
+	}
+}
+
